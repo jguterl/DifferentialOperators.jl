59f0126ebee42b5410d98515405510599313838e
Federico Halpern <halpernf@fusion.gat.com>
2024-02-13 18:04:03 -0800

    First commit of the (forward) staggered operator

--- src/example.jl
+++ src/example.jl
@@ -1,59 +1,72 @@
 using DifferentialOperators
 using BenchmarkTools
+
+# Grid sizes
+nx, ny = 128, 256
+
 # First we create a grid 
-grid = Grid(100, 100; L=[10.0, 2.0])
+grid = Grid(nx, ny; L=[2π, 4π])
 
 # We also define the data needed to calculated the derivatives (we can define order of accuracy here)
 grid_data = GridDerivatives(grid);
 
 # define a vector B  with some non-uniform value
 B = VectorField(grid);
-@. B.y.data = exp(-grid.y - grid.x);
-@. B.x.data = exp(-grid.y - grid.x);
-@. B.z.data = exp(-grid.y - grid.x);
+@. B.x.data = cos(1*grid.y) * cos(1*grid.x);
+@. B.y.data = cos(2*grid.y) * cos(2*grid.x);
+@. B.z.data = cos(3*grid.y) * cos(3*grid.x);
 
-nx, ny = size(grid)
-#a wrapper for testing (f is the operator we are interested in)
+
+
+# A wrapper for testing (f is the operator we are interested in)
 test!(f, result)  = compute!(f, grid_data, result, 3:nx-3, 3:ny-2)
 test_threads!(f, result)  = compute_threads!(f, grid_data, result, 3:nx-3, 3:ny-2)
-#Let's do a series of test 
+# Let's do a series of test 
 # First a simple product scalar times B
 η = 10.0;
 f1 = (η × B);
 r1 = VectorField(grid) # this is the result of the operator applied onto B or whatever field in the definition of f
+print("Test η×B\n")
 @btime test!($f1, $r1)
 
-# Thne the curl
-f2 = ((∇ × B));
+# Then the curl
+f2 = ((∇⁺ × B));
 r2 = VectorField(grid)
+print("Test ∇⁺×B\n")
 @btime test!($f2, $r2)
 
 # Combine 1 and 2 
 f3 = (η × (∇ × B))
 r3 = VectorField(grid)
+print("Test η×∇×B\n")
 @btime test!($f3, $r3)
 
 # Finally something useful...
 f4 = ∇ × (η × (∇ × B))
 r4 = VectorField(grid)
+print("Test ∇×η×∇×B\n")
 @btime test!($f4, $r4)
 
 # We can also compose at will
 f4 = ∇ × (f3)
 r4 = VectorField(grid)
+print("Test composition ∇×(previous calculation)\n")
 @btime test!($f4, $r4)
 
 # Some acceleration...
 f4 = ∇ × (η × (∇ × B))
 r4 = VectorField(grid)
+print("Test composition ∇×(previous calculation), threaded\n")
 @btime test_threads!($f4, $r4)
 #let's also do a gradient for fun
 f5 = ∇(B)
 r5 = VectorField(grid)
+print("Test ∇(B)\n")
 @btime test!($f5, $r5)
 
 f6 = ∇⋅(B)
 r6 = ScalarField(grid)
+print("Test ∇⋅(B)\n")
 @btime test!($f6, $r6)
 # the final call to the operator is inline and can be adjusted. It requires data for differentiation.
 # This can be dispatched to setup different order of differentation 
--- src/operators.jl
+++ src/operators.jl
@@ -1,8 +1,11 @@
-struct CurlOperator <: Operator end
-struct GradientOperator <: Operator end
-struct ProductOperator <: Operator end
-struct LaplacienOperator <: Operator end
-struct DivergenceOperator <: Operator end 
+struct CurlOperator       <: Operator end
+struct GradientOperator   <: Operator end
+struct Curl⁺Operator      <: Operator end
+struct Gradient⁺Operator  <: Operator end
+struct ProductOperator    <: Operator end
+struct LaplacienOperator  <: Operator end
+struct DivergenceOperator <: Operator end
+
 # multiply
 (op::ApplyOperator{D,V,ProductOperator,XComponent})(args...) where {D<:Float64,V} = op.var.x(args...) * op.data
 (op::ApplyOperator{D,V,ProductOperator,YComponent})(args...) where {D<:Float64,V} = op.var.y(args...) * op.data
@@ -10,47 +13,91 @@ struct DivergenceOperator <: Operator end
 
 #curl
 (op::ApplyOperator{D,V,CurlOperator,XComponent})(args...) where {D,V} = (∂y(op.var.z, args...) - ∂z(op.var.y, args...))
-(op::ApplyOperator{D,V,CurlOperator,YComponent})(args...) where {D,V} = (∂x(op.var.z, args...) - ∂z(op.var.x, args...))
+(op::ApplyOperator{D,V,CurlOperator,YComponent})(args...) where {D,V} = (∂z(op.var.x, args...) - ∂x(op.var.z, args...))
 (op::ApplyOperator{D,V,CurlOperator,ZComponent})(args...) where {D,V} = (∂x(op.var.y, args...) - ∂y(op.var.x, args...))
 
-#gradient
+#gradient --- warning, this should work for scalars, what does op.var contain?? Right now this is like diag( grad( V ) )
 (op::ApplyOperator{D,V,GradientOperator,XComponent})(args...) where {D,V} = ∂x(op.var.x, args...)
-(op::ApplyOperator{D,V,GradientOperator,YComponent})(args...) where {D,V} = ∂z(op.var.y, args...)
+(op::ApplyOperator{D,V,GradientOperator,YComponent})(args...) where {D,V} = ∂y(op.var.y, args...)
 (op::ApplyOperator{D,V,GradientOperator,ZComponent})(args...) where {D,V} = ∂z(op.var.z, args...)
 
-#laplacien
+#curl+
+(op::ApplyOperator{D,V,Curl⁺Operator,XComponent})(args...) where {D,V} = (∂y⁺(op.var.z, args...) - ∂z⁺(op.var.y, args...))
+(op::ApplyOperator{D,V,Curl⁺Operator,YComponent})(args...) where {D,V} = (∂z⁺(op.var.x, args...) - ∂x⁺(op.var.z, args...))
+(op::ApplyOperator{D,V,Curl⁺Operator,ZComponent})(args...) where {D,V} = (∂x⁺(op.var.y, args...) - ∂y⁺(op.var.x, args...))
+
+#gradient+ --- warning, this should work for scalars, what does op.var contain?? Right now this is like diag( grad( V ) )
+(op::ApplyOperator{D,V,Gradient⁺Operator,XComponent})(args...) where {D,V} = ∂x⁺(op.var.x, args...)
+(op::ApplyOperator{D,V,Gradient⁺Operator,YComponent})(args...) where {D,V} = ∂y⁺(op.var.y, args...)
+(op::ApplyOperator{D,V,Gradient⁺Operator,ZComponent})(args...) where {D,V} = ∂z⁺(op.var.z, args...)
+
+#laplacien --- TODO: eventually this should overloaded as psi = div-( grad+( phi ) or A = - curl-( curl+ ( B ) + grad-(div+( B ) )
 (op::ApplyOperator{D,V,LaplacienOperator,XComponent})(args...) where {D,V} = ∂x²(op.var.x, args...) + ∂y²(op.var.x, args...) + ∂z²(op.var.x, args...)
 (op::ApplyOperator{D,V,LaplacienOperator,YComponent})(args...) where {D,V} = ∂x²(op.var.y, args...) + ∂y²(op.var.y, args...) + ∂z²(op.var.y, args...)
 (op::ApplyOperator{D,V,LaplacienOperator,ZComponent})(args...) where {D,V} = ∂x²(op.var.z, args...) + ∂y²(op.var.z, args...) + ∂z²(op.var.z, args...)
 
-#laplacien
-(op::ApplyOperator{D,V,DivergenceOperator,ScalarComponent})(args...) where {D,V} = ∂x(op.var.x, args...) + ∂y(op.var.x, args...) + ∂z(op.var.x, args...)
+#divergence
+(op::ApplyOperator{D,V,DivergenceOperator,ScalarComponent})(args...) where {D,V} = ∂x(op.var.x, args...) + ∂y(op.var.y, args...) + ∂z(op.var.z, args...)
 
 # TODO: dispatch on derivative accuracy orders
-∂x(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i + 1, j) - v(i, j)) / grid_data.dx(i, j)
-∂y(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j + 1) - v(i, j)) / grid_data.dy(i, j)
+
+#Centered differences
+∂x(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (v(i + 1, j) - v(i - 1, j)) / grid_data.dx(i, j)
+∂y(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (v(i, j + 1) - v(i, j - 1)) / grid_data.dy(i, j)
 ∂z(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
 
+#Forward staggered
+∂x⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i + 1, j) - v(i, j)) / grid_data.dx(i, j)
+∂y⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j + 1) - v(i, j)) / grid_data.dy(i, j)
+∂z⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
 
-∂x(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i + 1, j) - op(grid_data, i, j)) / grid_data.dx(i, j)
-∂y(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j + 1) - op(grid_data, i, j)) / grid_data.dy(i, j)
+#Backward staggered
+∂x⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j) - v(i - 1, j)) / grid_data.dx(i, j)
+∂y⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j) - v(i, j - 1)) / grid_data.dy(i, j)
+∂z⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
+
+# This looks like operations for stretched grids -- I'll just replicate for ∂x⁺, ∂x⁻, etc but I'll ask JG
+∂x(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (op(grid_data, i + 1, j) - op(grid_data, i - 1, j)) / grid_data.dx(i, j)
+∂y(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (op(grid_data, i, j + 1) - op(grid_data, i, j - 1)) / grid_data.dy(i, j)
 ∂z(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
 
+∂x⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i + 1, j) - op(grid_data, i, j)) / grid_data.dx(i, j)
+∂y⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j + 1) - op(grid_data, i, j)) / grid_data.dy(i, j)
+∂z⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
+
+∂x⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j) - op(grid_data, i - 1, j)) / grid_data.dx(i, j)
+∂y⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j) - op(grid_data, i, j - 1)) / grid_data.dy(i, j)
+∂z⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
 
 abstract type AbstractOperator{D,V,O<:Operator} end 
-Curl{D,V} = AbstractOperator{D,V,CurlOperator}
-Gradient{D,V} = AbstractOperator{D,V,GradientOperator}
-∇{D,V} = AbstractOperator{D,V,GradientOperator}
-Product{D,V} = AbstractOperator{D,V,ProductOperator}
-
+#Centered operators
+Curl{D,V}         = AbstractOperator{D,V,CurlOperator}
+Gradient{D,V}     = AbstractOperator{D,V,GradientOperator}
+∇{D,V}            = AbstractOperator{D,V,GradientOperator}
 ∇(v::VectorField) = VectorField(nothing, v, GradientOperator())
-Curl(v) = Curl(nothing, v)
-Curl(d, v) = VectorField(d, v, CurlOperator())
 
-×(::Type{∇}, v::VectorField) = Curl(v)
+Curl(v)           = Curl(nothing, v)
+Curl(d, v)        = VectorField(d, v, CurlOperator())
+
+
+#Forward stagged operators
+Curl⁺{D,V}         = AbstractOperator{D,V,Curl⁺Operator}
+Gradient⁺{D,V}     = AbstractOperator{D,V,Gradient⁺Operator}
+∇⁺{D,V}            = AbstractOperator{D,V,Gradient⁺Operator}
+∇⁺(v::VectorField) = VectorField(nothing, v, GradientOperator())
+
+Curl⁺(v)           = Curl⁺(nothing, v)
+Curl⁺(d, v)        = VectorField(d, v, Curl⁺Operator())
+
+Product{D,V}  = AbstractOperator{D,V,ProductOperator}
+
+
+×( ::Type{∇⁺}, v ::VectorField) = Curl⁺(v)
+×( ::Type{∇} , v ::VectorField) = Curl(v)
 ×(s::Float64, var::VectorField) = VectorField(s, var, ProductOperator())
-⋅(::Type{∇}, var::VectorField) = ScalarField(nothing, var, DivergenceOperator())
-VectorField(d, v, o::Operator) = VectorField(ApplyOperatorX(d, v, o), ApplyOperatorY(d, v, o), ApplyOperatorZ(d, v, o))
-ScalarField(d, v, o::Operator) = ScalarField(ApplyOperatorScalar(d, v, o))
+⋅( ::Type{∇}, var::VectorField) = ScalarField(nothing, var, DivergenceOperator())
 
-export ∇, ×, ⋅
+VectorField(d, v, o::Operator)  = VectorField(ApplyOperatorX(d, v, o), ApplyOperatorY(d, v, o), ApplyOperatorZ(d, v, o))
+ScalarField(d, v, o::Operator)  = ScalarField(ApplyOperatorScalar(d, v, o))
+
+export ∇, ∇⁺, ×, ⋅
