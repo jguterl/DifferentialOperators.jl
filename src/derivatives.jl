20ee0b59f181aa06f8ab3a92cbdc3186745f0795
jguterl <guterlj@fusion.gat.com>
2024-02-20 17:08:42 -0800

    fix derivate to be done (NOT DONE)

--- src/derivatives.jl
+++ src/derivatives.jl
@@ -32,6 +32,42 @@
 ∂z⁻(op::Union{ApplyOperator,FieldData}, grid_data::AbstractGridDerivatives, i::Index, j::Index) = 0.0 #zero_value_array(grid_data,i,j)
 
 
+# Fix federico  -> 
+#TODO set dx[i,j] for i,j in ghoest cell = dx[boundary] (see grid.jl for that fix)
+# ∂x(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (v(i + 1, j) - v(i - 1, j)) / grid_data.dx(3, 3)
+# ∂y(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (v(i, j + 1) - v(i, j - 1)) / grid_data.dy(3, 3)
+# ∂z(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
+
+# #Centered differences
+# ∂x²(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i + 1, j) - 2 * v(i, j) + v(i - 1, j)) / grid_data.dx(3, 3)^2
+# ∂y²(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j + 1) - 2 * v(i, j) + v(i, j - 1)) / grid_data.dy(3, 3)^2
+# ∂z²(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
+
+# #Forward staggered
+# ∂x⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i + 1, j) - v(i, j)) / grid_data.dx(3, 3)
+# ∂y⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j + 1) - v(i, j)) / grid_data.dy(3, 3)
+# ∂z⁺(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
+
+# #Backward staggered
+# ∂x⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j) - v(i - 1, j)) / grid_data.dx(3, 3)
+# ∂y⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (v(i, j) - v(i, j - 1)) / grid_data.dy(3, 3)
+# ∂z⁻(v::FieldData, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0
+
+# # This looks like operations for stretched grids -- I'll just replicate for ∂x⁺, ∂x⁻, etc but I'll ask JG
+# ∂x(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (op(grid_data, i + 1, j) - op(grid_data, i - 1, j)) / grid_data.dx(3, 3)
+# ∂y(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.5 * (op(grid_data, i, j + 1) - op(grid_data, i, j - 1)) / grid_data.dy(3, 3)
+# ∂z(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
+
+# ∂x⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i + 1, j) - op(grid_data, i, j)) / grid_data.dx(3, 3)
+# ∂y⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j + 1) - op(grid_data, i, j)) / grid_data.dy(3, 3)
+# ∂z⁺(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
+
+# ∂x⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j) - op(grid_data, i - 1, j)) / grid_data.dx(3, 3)
+# ∂y⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = (op(grid_data, i, j) - op(grid_data, i, j - 1)) / grid_data.dy(3, 3)
+# ∂z⁻(op::ApplyOperator, grid_data::AbstractGridDerivatives, i::Int64, j::Int64) = 0.0
+
+
+
 struct StencilCoefficients{V,W,Z, dX,dY,dZ} <: AbstractStencilCoefficients{dX,dY,dZ}
     α_x :: V
     α_y :: W
